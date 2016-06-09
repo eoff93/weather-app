43b96028e43ef8f128feaa00c2a8346fb8a994c8
eoff93 <bane.dev101@gmail.com>
2016-06-09 22:36:09 +0200

    Add a function that sets the api

--- js/main.es6
+++ js/main.es6
@@ -3,3 +3,8 @@ let lon = 0;
 const apiId = 'e28ac852596e204e2ab1e5198ae5bbf3';
 let api;
 let iconUrl = undefined;
+
+// sets the api variable to the whole api key
+function setApi() {
+  api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&APPID=${apiId}`;
+}
--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ var lon = 0;
 var apiId = 'e28ac852596e204e2ab1e5198ae5bbf3';
 var api = void 0;
 var iconUrl = undefined;
+
+// sets the api variable to the whole api key
+function setApi() {
+  api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&APPID=' + apiId;
+}
