884518f2e22cd98274309a6dca60e0edbbf8c23a
eoff93 <bane.dev101@gmail.com>
2016-06-09 22:32:20 +0200

    Define needed variables

---
+++ js/main.es6
@@ -0,0 +1,5 @@
+let lat = 0;
+let lon = 0;
+const apiId = 'e28ac852596e204e2ab1e5198ae5bbf3';
+let api;
+let iconUrl = undefined;
--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-"use strict";
+'use strict';
+
+var lat = 0;
+var lon = 0;
+var apiId = 'e28ac852596e204e2ab1e5198ae5bbf3';
+var api = void 0;
+var iconUrl = undefined;
